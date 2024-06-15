be16c3fa12afdb5e329da946cb7678e20ca8da6c
dondycles <johnrod.dondoyano@gmail.com>
2024-06-16 01:48:47 +0800

    modified:   app/(user)/list/money.tsx

--- app/(user)/list/money.tsx
+++ app/(user)/list/money.tsx
@@ -12,8 +12,16 @@ import {
 import { deleteMoney } from "@/app/actions/moneys";
 import { useState } from "react";
 import { TbCurrencyPeso } from "react-icons/tb";
-import { Pencil, Trash } from "lucide-react";
+import { Check, Pencil, Trash, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DialogDescription } from "@radix-ui/react-dialog";
 
 export default function Money({
   money,
@@ -28,10 +36,9 @@ export default function Money({
   hideAmounts: boolean;
   currentTotal: string;
 }) {
-  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
   const [isPending, setIsPending] = useState(false);
   const [elevate, setElevate] = useState(false);
-
   const handleDelete = async () => {
     setIsPending(true);
     const { error } = await deleteMoney(money, currentTotal);
@@ -40,35 +47,81 @@ export default function Money({
   };
 
   return (
-    <ContextMenu onOpenChange={setElevate}>
-      <ContextMenuTrigger>
-        <div
-          key={money.id}
-          className={`p-2 border rounded-lg flex flex-row justify-between items-center font-bold ${
-            isPending && "opacity-50 pointer-events-none "
-          } ${
-            elevate ? "shadow-lg scale-[100.5%]" : "shadow-none"
-          } ease-in-out transition-all`}
-        >
-          <p className="truncate">{money.name}</p>
-          <div className="font-semibold font-anton flex items-center">
-            <TbCurrencyPeso />
-            {hideAmounts
-              ? AsteriskNumber(money.amount ?? 0)
-              : UsePhpPeso(money.amount ?? 0)}
+    <Dialog key={money.id} open={showWarning} onOpenChange={setShowWarning}>
+      <ContextMenu key={money.id} onOpenChange={setElevate}>
+        <ContextMenuTrigger>
+          <div
+            key={money.id}
+            className={`p-2 border rounded-lg flex flex-row justify-between items-center font-bold ${
+              isPending && "opacity-50 pointer-events-none "
+            } ${
+              elevate ? "shadow-lg scale-[100.5%]" : "shadow-none"
+            } ease-in-out transition-all`}
+          >
+            <p className="truncate">{money.name}</p>
+            <div className="font-semibold font-anton flex items-center">
+              <TbCurrencyPeso />
+              {hideAmounts
+                ? AsteriskNumber(money.amount ?? 0)
+                : UsePhpPeso(money.amount ?? 0)}
+            </div>
           </div>
-        </div>
-      </ContextMenuTrigger>
-      <ContextMenuContent>
-        <ContextMenuItem onClick={() => edit()} className="text-xs">
-          <Pencil className="size-4 mr-1" />
-          Edit
-        </ContextMenuItem>
-        <ContextMenuItem className="text-xs">
-          <Trash className="size-4 mr-1 text-destructive" />
-          <span className="text-destructive">Delete</span>
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+        </ContextMenuTrigger>
+        <ContextMenuContent>
+          <ContextMenuItem onClick={() => edit()} className="text-xs">
+            <Pencil className="size-4 mr-1" />
+            Edit
+          </ContextMenuItem>
+          <ContextMenuItem className="text-xs" asChild>
+            <DialogTrigger className="w-full">
+              <Trash className="size-4 mr-1 text-destructive" />
+              <span className="text-destructive">Delete</span>
+            </DialogTrigger>
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+      <DialogContent className="p-2 w-fit">
+        <DialogHeader>
+          <DialogTitle className="text-destructive text-center">
+            Warning!
+          </DialogTitle>
+          <DialogDescription>Are you sure to delete?</DialogDescription>
+          <div
+            key={money.id}
+            className={`p-2 border rounded-lg flex flex-row justify-between items-center font-bold ${
+              isPending && "opacity-50 pointer-events-none "
+            } ${
+              elevate ? "shadow-lg scale-[100.5%]" : "shadow-none"
+            } ease-in-out transition-all`}
+          >
+            <p className="truncate">{money.name}</p>
+            <div className="font-semibold font-anton flex items-center">
+              <TbCurrencyPeso />
+              {hideAmounts
+                ? AsteriskNumber(money.amount ?? 0)
+                : UsePhpPeso(money.amount ?? 0)}
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <Button
+              disabled={isPending}
+              onClick={handleDelete}
+              className="flex-1"
+              variant={"destructive"}
+            >
+              <Check className="size-4" />
+            </Button>
+            <Button
+              disabled={isPending}
+              onClick={() => setShowWarning(false)}
+              className="flex-1"
+              variant={"outline"}
+            >
+              <X className="size-4" />
+            </Button>
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
