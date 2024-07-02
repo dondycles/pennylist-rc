a851a527ca948bf36f436dfc0d8a130d785ea49b
dondycles <johnrod.dondoyano@gmail.com>
2024-07-02 21:35:33 +0800

    modified:   tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,6 +81,9 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
 } satisfies Config;
 
 export default config;
